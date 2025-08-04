de2102d2ad7aa829ef2ee2ac9970a0e68c69941d
CmrCrabs <zayaanazam1234@hotmail.com>
2025-08-04 11:20:10 +0100

    merw

--- flake.nix
+++ flake.nix
@@ -7,37 +7,38 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, neovim-nightly, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { 
-					inherit system; 
-					overlays = [
-            neovim-nightly.overlays.default
-					];
-				};
+  outputs = {
+    self,
+    nixpkgs,
+    neovim-nightly,
+    flake-utils,
+    ...
+  }:
+  # flake-utils.lib.eachDefaultSystem (system:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        neovim-nightly.overlays.default
+      ];
+    };
 
-				#storedConfig = pkgs.runCommand "stored-nvim-config" {
-				#		buildInputs = [ ];
-        #  } ''
-        #    mkdir -p $out/nvim
-        #    cp -r ${./config/nvim}/* $out/
-				#'';
+    tempDir =
+      pkgs.runCommand "initFolder" {
+      } ''
+        mkdir -p $out
+             cp -r ${self}/config/nvim $out/
+      '';
 
-        nnvimWrapped = pkgs.writeShellApplication {
-          name = "nnvim";
-          runtimeInputs = [ pkgs.neovim ];
-          text = ''
-            export XDG_CONFIG_HOME="./config"
-            exec nvim "$@"
-          '';
-        };
-
-      in {
-        packages.default = nnvimWrapped;
-				apps.default = {
-          type = "app";
-          program = "${nnvimWrapped}/bin/nnvim";
-        };
-      });
+    nvimWrap = pkgs.writeShellApplication {
+      name = "nvimWrap";
+      runtimeInputs = [pkgs.neovim];
+      text = ''
+        exec nvim -u ${tempDir}/nvim/init.lua
+      '';
+    };
+  in {
+    packages.${system}.default = nvimWrap;
+  };
 }
